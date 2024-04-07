5cf735ddc192cab68c4b557a05d4fa442c298c79
Coco <cocochen12301@gmail.com>
2024-04-06 19:53:56 -0700

    Initial commit

---
+++ src/main/java/com/wordlewithfriends/wwf/AuthUser.java
@@ -0,0 +1,20 @@
+package com.wordlewithfriends.wwf;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document("user")
+public class AuthUser {
+    @Id
+    private String id;
+    @Indexed
+    private String username;
+    private String password;
+    private boolean active;
+
+}
